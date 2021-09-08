fc3fc0e40763009a8f1fab4d4dd900d502171739
BB9z <bb9z@me.com>
2021-09-08 23:12:55 +0800

    Define swift package.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LAME",
+    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)],
+    products: [
+        .library(name: "LAME", targets: ["LAME"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "LAME",
+            path: "macosx/LAME.xcframework"
+        )
+    ]
+)
