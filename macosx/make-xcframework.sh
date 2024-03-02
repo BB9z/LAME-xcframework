39b39dcdf938d11df737696d2e357a9f618e142c
BB9z <bb9z@me.com>
2024-03-02 23:29:58 +0800

    Move BUILD_LIBRARY_FOR_DISTRIBUTION to xcconfig.

--- macosx/make-xcframework.sh
+++ macosx/make-xcframework.sh
@@ -21,57 +21,49 @@ xcodebuild archive \
     -scheme LAME-macOS \
     -destination "generic/platform=macOS" \
     -archivePath "build/macOS" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    SKIP_INSTALL=NO
 
 xcodebuild archive \
     -scheme LAME-iOS \
     -destination "generic/platform=iOS" \
     -archivePath "build/iOS" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    SKIP_INSTALL=NO
 
 xcodebuild archive \
     -scheme LAME-iOS \
     -destination "generic/platform=iOS Simulator" \
     -archivePath "build/iOS-Simulator" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    SKIP_INSTALL=NO
 
 xcodebuild archive \
     -scheme LAME-iOS \
     -destination "generic/platform=macOS,variant=Mac Catalyst" \
     -archivePath "build/Catalyst" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    SKIP_INSTALL=NO
 
 xcodebuild archive \
     -scheme LAME-tvOS \
     -destination "generic/platform=tvOS" \
     -archivePath "build/tvOS" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    SKIP_INSTALL=NO
 
 xcodebuild archive \
     -scheme LAME-tvOS \
     -destination "generic/platform=tvOS Simulator" \
     -archivePath "build/tvOS-Simulator" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    SKIP_INSTALL=NO
 
 xcodebuild archive \
     -scheme LAME-watchOS \
     -destination "generic/platform=watchOS" \
     -archivePath "build/watchOS" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    SKIP_INSTALL=NO
 
 xcodebuild archive \
     -scheme LAME-watchOS \
     -destination "generic/platform=watchOS Simulator" \
     -archivePath "build/watchOS-Simulator" \
-    SKIP_INSTALL=NO \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    SKIP_INSTALL=NO
 
 xcodebuild -create-xcframework \
     -framework "build/macOS.xcarchive/Products/Library/Frameworks/LAME.framework" \
