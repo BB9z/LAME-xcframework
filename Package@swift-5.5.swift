cd59f211cfbc0dc3855a26a11a1bc61c5dd41ecc
BB9z <bb9z@me.com>
2024-03-03 00:05:48 +0800

    Add version packages.

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,21 +11,20 @@ let package = Package(
         .macOS(.v10_13),
         .tvOS(.v12),
         .watchOS(.v4),
-        .visionOS(.v1),
     ],
     products: [
         .library(name: "LAME", targets: ["LAME"])
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(
-            name: "LAME",
-            path: "macosx/LAME.xcframework"
-        ),
 //        .binaryTarget(
 //            name: "LAME",
-//            url: "https://github.com/BB9z/LAME-xcframework/releases/download/3.100.1/LAME.xcframework.zip",
-//            checksum: "9f1106a7ace15bcdb40106e6e5c7edbd77aa68569e7c4537c50bb566b29973e5"
-//        )
+//            path: "macosx/LAME.xcframework"
+//        ),
+        .binaryTarget(
+            name: "LAME",
+            url: "https://github.com/BB9z/LAME-xcframework/releases/download/3.100.1/LAME.xcframework.zip",
+            checksum: "9f1106a7ace15bcdb40106e6e5c7edbd77aa68569e7c4537c50bb566b29973e5"
+        )
     ]
 )
