1b154defbba87112c5eab9dfbbabe0dcc16e42d5
BB9z <bb9z@me.com>
2024-03-02 23:57:43 +0800

    Update package version.

--- Package.swift
+++ Package.swift
@@ -5,19 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "LAME",
-    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)],
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .tvOS(.v12),
+        .watchOS(.v4),
+    ],
     products: [
         .library(name: "LAME", targets: ["LAME"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // .binaryTarget(
-        //     name: "LAME",
-        //     path: "macosx/LAME.xcframework"
-        // ),
+//        .binaryTarget(
+//            name: "LAME",
+//            path: "macosx/LAME.xcframework"
+//        ),
        .binaryTarget(
            name: "LAME",
            url: "https://.../LAME.xcframework.zip",
